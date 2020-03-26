f73153d558879aa24fb62d5a6d191f02f0aa16e7
Jason Franklin <jfrank325@gmail.com>
2020-03-26 15:53:46 +0100

    Finished task

--- src/App.js
+++ src/App.js
@@ -1,80 +1,88 @@
 import React from 'react';
-import result from './data.json'
-import InputField from './InputField'
-import SearchButton from './SearchButton'
-import ShowResults from './ShowResults'
-import Grid from '@material-ui/core/Grid'
+import result from './data.json';
+import InputField from './InputField';
+import SearchButton from './SearchButton';
+import ShowResults from './ShowResults';
+import Grid from '@material-ui/core/Grid';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import SwitchStyle from './SwitchStyle';
 
 const theme = createMuiTheme({
-  palette:{
+  palette: {
     primary: {
-      main:'#007BA7',
+      main: '#007BA7',
     },
-    secondary:{
-      main:'#02e2f2'
-    }
+    secondary: {
+      main: '#02e2f2',
+    },
   },
   typography: {
-    fontFamily: [
-      'Nunito',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
+    fontFamily: ['Nunito', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+  },
 });
 
 class App extends React.Component {
-  state={
-    inputSphere:0,
-    results:[],
+  state = {
+    inputSphere: 0,
+    inputCylinder: 0,
+    inputAddition: 0,
+    results: [],
     darkMode: false,
-  }
-  findResults=(inputSphere)=>{
-    this.setState({results:result.data})
-  }
-  onSearch=()=>{
-    console.log("to be done")
-  }
-  onChange=(key,value)=>{
-    this.setState({[key]:value})
-  }
-  toggledarkMode=()=>{
+  };
+
+  componentDidMount() {
     this.setState({
-      darkMode: !this.state.darkMode
-    })
+      results: result.data,
+    });
   }
-  render (){
-    const { darkMode, inputSphere, results } = this.state;
-    return(
+
+  findResults = (inputSphere, inputCylinder, inputAddition) => {
+    const filtered = result.data.filter(item => {
+      console.log(result.data, 'this is my result');
+      return (
+        inputSphere >= item.minSphere &&
+        inputSphere <= item.maxSphere &&
+        inputCylinder >= item.minCylinder &&
+        inputCylinder <= item.maxCylinder &&
+        inputAddition >= item.minAddition &&
+        inputAddition <= item.maxAddition
+      );
+    });
+
+    this.setState({ results: filtered });
+    console.log(result, 'first console log');
+  };
+  onSearch = () => {
+    const { inputSphere, inputCylinder, inputAddition } = this.state;
+    this.findResults(inputSphere, inputCylinder, inputAddition);
+    console.log('to be done');
+  };
+  onChange = (key, value) => {
+    this.setState({ [key]: value });
+  };
+  toggledarkMode = () => {
+    this.setState({
+      darkMode: !this.state.darkMode,
+    });
+  };
+  render() {
+    const { darkMode, inputSphere, inputCylinder, inputAddition, results } = this.state;
+    return (
       <ThemeProvider theme={theme}>
-        <div className={darkMode?"AppDark":"AppLight"}>
-          <Grid className='searchField'>
-          <InputField
-            inputField={inputSphere}
-            name="inputSphere"
-            label="Sphere"
-            onChange={this.onChange}
-          />
-          <SearchButton
-            onSearch={this.onSearch}
-          />
+        <div className={darkMode ? 'AppDark' : 'AppLight'}>
+          <Grid className="searchField">
+            <InputField inputField={inputSphere} name="inputSphere" label="Sphere" onChange={this.onChange} />
+            <InputField inputField={inputCylinder} name="inputCylinder" label="Cylinder" onChange={this.onChange} />
+            <InputField inputField={inputAddition} name="inputAddition" label="Addition" onChange={this.onChange} />
+            <SearchButton onSearch={this.onSearch} />
           </Grid>
-          <Grid className={darkMode?'serchResult':'serchResultLight'}>
-          <ShowResults
-            results={results}
-          />
+          <Grid className={darkMode ? 'serchResult' : 'serchResultLight'}>
+            <ShowResults results={results} />
           </Grid>
-          <SwitchStyle
-            darkMode={this.state.darkMode}
-            toggledarkMode={this.toggledarkMode}
-          />
+          <SwitchStyle darkMode={this.state.darkMode} toggledarkMode={this.toggledarkMode} />
         </div>
       </ThemeProvider>
-    )
+    );
   }
 }
 
